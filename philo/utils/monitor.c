54d128b044ea02fa1b2429b0b4d738fd1e8ed3d4
hlibine <hugo.libine@gmail.com$>
2024-04-05 11:41:16 +0200

    updated monitor.c

--- philo/utils/monitor.c
+++ philo/utils/monitor.c
@@ -6,15 +6,22 @@
 /*   By: hlibine <hlibine@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 13:11:15 by hlibine           #+#    #+#             */
-/*   Updated: 2024/04/04 14:29:36 by hlibine          ###   ########.fr       */
+/*   Updated: 2024/04/05 11:39:56 by hlibine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../philosophers.h"
 
-static int	check_starving(t_philo *philo)
+static void	kill_philos(t_core *core, t_philo *philo)
 {
 	
+}
+
+static int	check_starving(t_philo *philo)
+{
+	if (get_current_time() - philo->last_meal <= philo->core->time_to_die)
+		return (1);
+	return (0);
 }
 
 static int	check_done(t_philo *philo)
@@ -40,7 +47,7 @@ void	*ph_monitor(void *in)
 			if (!checkdeath(core->philos[i], core))
 				return (0);
 			if (!check_done(core->philos[i]) && check_straving(core->philos[i]))
-				
+				kill_philos(core, core->philos[i]);
 		}
 		ph_usleep(1);
 	}
