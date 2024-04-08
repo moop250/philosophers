52de8d5376076ea9da59ec3906ec6064d4bc7eeb
hlibine <hugo.libine@gmail.com>
2024-04-08 13:25:43 +0200

    mac compatibility

--- philo/philosophers.c
+++ philo/philosophers.c
@@ -6,7 +6,7 @@
 /*   By: hlibine <hlibine@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/20 16:42:18 by hlibine           #+#    #+#             */
-/*   Updated: 2024/04/06 00:47:23 by hlibine          ###   ########.fr       */
+/*   Updated: 2024/04/08 11:58:21 by hlibine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,10 @@ static void	free_philos(t_core *core)
 	i = -1;
 	while (core->philos[++i])
 	{
-		if (&core->philos[i]->r_fork)
-			pthread_mutex_destroy(&core->philos[i]->r_fork);
-		if (&core->philos[i]->meal_lock)
-			pthread_mutex_destroy(&core->philos[i]->meal_lock);
-		if (&core->philos[i]->hunger_lock)
-			pthread_mutex_destroy(&core->philos[i]->hunger_lock);
-		if (&core->philos[i]->lml)
-			pthread_mutex_destroy(&core->philos[i]->lml);
+		pthread_mutex_destroy(&core->philos[i]->r_fork);
+		pthread_mutex_destroy(&core->philos[i]->meal_lock);
+		pthread_mutex_destroy(&core->philos[i]->hunger_lock);
+		pthread_mutex_destroy(&core->philos[i]->lml);
 		free(core->philos[i]);
 	}
 	free(core->philos);
@@ -35,13 +31,12 @@ static void	free_philos(t_core *core)
 static void	free_core(t_core *core)
 {
 	if (core->philos)
+	{
 		free_philos(core);
-	if (&core->death_lock)
 		pthread_mutex_destroy(&core->death_lock);
-	if (&core->write_lock)
 		pthread_mutex_destroy(&core->write_lock);
-	if (&core->monitor_lock)
 		pthread_mutex_destroy(&core->monitor_lock);
+	}
 	free(core);
 }
 
